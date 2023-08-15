6f2bce272a05615435e250bd48d0086c5b638012
Shimeji996 <k022g0021@g.neec.ac.jp>
2023-08-15 22:49:50 +0900

    02_05終わり

--- Engine.cpp
+++ Engine.cpp
@@ -1,73 +1,76 @@
 ﻿#include "Engine.h"
 #include <assert.h>
 
-IDxcBlob* MyEngine::CompileShader(const std::wstring& filePath, const wchar_t* profile, IDxcUtils* dxcUtils, IDxcCompiler3* dxcCompiler, IDxcIncludeHandler* includeHandler)
-{
-	//これからシェーダーをコンパイルする旨をログに出す
-	Log(ConvertString(std::format(L"Begin CompileShader, path:{},profile:{}\n", filePath, profile)));
+IDxcBlob* MyEngine::CompileShader(
+	const std::wstring& filePath, const wchar_t* profile, IDxcUtils* dxcUtils,
+	IDxcCompiler3* dxcCompiler, IDxcIncludeHandler* includeHandler) {
+	// これからシェーダーをコンパイルする旨をログに出す
+	Log(ConvertString(
+		std::format(L"Begin CompileShader, path:{},profile:{}\n", filePath, profile)));
 
-	//hlslファイルを読む
+	// hlslファイルを読む
 	IDxcBlobEncoding* shaderSource = nullptr;
 	dxCommon_->SetHr(dxcUtils->LoadFile(filePath.c_str(), nullptr, &shaderSource));
 
-	//読めなかったら決める
+	// 読めなかったら決める
 	assert(SUCCEEDED(dxCommon_->GetHr()));
 
-	//読み込んだファイルの内容を設定する
+	// 読み込んだファイルの内容を設定する
 	DxcBuffer shaderSourceBuffer;
 	shaderSourceBuffer.Ptr = shaderSource->GetBufferPointer();
 	shaderSourceBuffer.Size = shaderSource->GetBufferSize();
 	shaderSourceBuffer.Encoding = DXC_CP_UTF8;
 	LPCWSTR arguments[] = {
-		filePath.c_str(),//コンパイル対象のhlslファイル名
-		L"-E",L"main",//エントリーポイントの指定。基本的にmain以外にはしない
-		L"-T",profile,//ShaderProflieの設定
-		L"-Zi",L"-Qembed_debug",//デバッグ用の情報を埋め込む
-		L"-Od", //最適化を外しておく
-		L"-Zpr",//メモリレイアウトは行優先
+		filePath.c_str(), // コンパイル対象のhlslファイル名
+		L"-E",
+		L"main", // エントリーポイントの指定。基本的にmain以外にはしない
+		L"-T",
+		profile, // ShaderProflieの設定
+		L"-Zi",
+		L"-Qembed_debug", // デバッグ用の情報を埋め込む
+		L"-Od",           // 最適化を外しておく
+		L"-Zpr",          // メモリレイアウトは行優先
 	};
 
-	//実際にShaderをコンパイルする
+	// 実際にShaderをコンパイルする
 	IDxcResult* shaderResult = nullptr;
 	dxCommon_->SetHr(dxcCompiler->Compile(
-		&shaderSourceBuffer,//読み込んだファイル
-		arguments,//コンパイルオプション
-		_countof(arguments),//コンパイルオプションの数
-		includeHandler, // includeが含まれた諸々
-		IID_PPV_ARGS(&shaderResult)//コンパイル結果
+		&shaderSourceBuffer,        // 読み込んだファイル
+		arguments,                  // コンパイルオプション
+		_countof(arguments),        // コンパイルオプションの数
+		includeHandler,             // includeが含まれた諸々
+		IID_PPV_ARGS(&shaderResult) // コンパイル結果
 	));
 
-	//コンパイルエラーではなくdxcが起動できないなど致命的な状況
+	// コンパイルエラーではなくdxcが起動できないなど致命的な状況
 	assert(SUCCEEDED(dxCommon_->GetHr()));
 
-	//警告・エラーが出たらログに出して止める
+	// 警告・エラーが出たらログに出して止める
 	IDxcBlobUtf8* shaderError = nullptr;
 	shaderResult->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&shaderError), nullptr);
-	if (shaderError != nullptr && shaderError->GetStringLength() != 0)
-	{
+	if (shaderError != nullptr && shaderError->GetStringLength() != 0) {
 		Log(shaderError->GetStringPointer());
-		//警告・エラーダメ絶対
+		// 警告・エラーダメ絶対
 		assert(false);
 	}
 
-	//コンパイル結果から実行用のバイナリ部分を取得
+	// コンパイル結果から実行用のバイナリ部分を取得
 	IDxcBlob* shaderBlob = nullptr;
 	dxCommon_->SetHr(shaderResult->GetOutput(DXC_OUT_OBJECT, IID_PPV_ARGS(&shaderBlob), nullptr));
 	assert(SUCCEEDED(dxCommon_->GetHr()));
 
-	//成功したログを出す
+	// 成功したログを出す
 	Log(ConvertString(std::format(L"Compile Succeeded, path:{},profile:{}\n", filePath, profile)));
 
-	//もう使わないリソースを開放
+	// もう使わないリソースを開放
 	shaderSource->Release();
 	shaderResult->Release();
 
-	//実行用のバイナリを返却
+	// 実行用のバイナリを返却
 	return shaderBlob;
 }
 
-void MyEngine::InitializeDxcCompiler()
-{
+void MyEngine::InitializeDxcCompiler() {
 	HRESULT hr;
 	dxcUtils_ = nullptr;
 	dxcCompiler_ = nullptr;
@@ -77,31 +80,29 @@ void MyEngine::InitializeDxcCompiler()
 	hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler_));
 	assert(SUCCEEDED(hr));
 
-	//現時点でincludeはしないが、includeに対応するための設定を行っていく
+	// 現時点でincludeはしないが、includeに対応するための設定を行っていく
 	includeHandler_ = nullptr;
 	hr = dxcUtils_->CreateDefaultIncludeHandler(&includeHandler_);
 	assert(SUCCEEDED(hr));
 }
 
-void MyEngine::CreateRootSignature()
-{
-	//RootSignature作成
+void MyEngine::CreateRootSignature() {
+	// RootSignature作成
 	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
-	descriptionRootSignature.Flags =
-		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+	descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 
-	//RootParameter作成、複数設定可能な為、配列に
+	// RootParameter作成、複数設定可能な為、配列に
 	D3D12_ROOT_PARAMETER rootParameters[3] = {};
-	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;//CBVを使う
-	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;//PixelShaderで使う
-	rootParameters[0].Descriptor.ShaderRegister = 0;//レジスタ番号0とバインド
-	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;//CBVを使う
-	rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;//VertexShaderで使う
-	rootParameters[1].Descriptor.ShaderRegister = 0;//レジスタ番号0とバインド
-	descriptionRootSignature.pParameters = rootParameters;//ルートパラメータ配列へのポインタ
-	descriptionRootSignature.NumParameters = _countof(rootParameters);//配列の長さ
+	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;    // CBVを使う
+	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
+	rootParameters[0].Descriptor.ShaderRegister = 0; // レジスタ番号0とバインド
+	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;     // CBVを使う
+	rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX; // VertexShaderで使う
+	rootParameters[1].Descriptor.ShaderRegister = 0; // レジスタ番号0とバインド
+	descriptionRootSignature.pParameters = rootParameters; // ルートパラメータ配列へのポインタ
+	descriptionRootSignature.NumParameters = _countof(rootParameters); // 配列の長さ
 
-	//DescriptorRange
+	// DescriptorRange
 	D3D12_DESCRIPTOR_RANGE descriptorRange[1] = {};
 	descriptorRange[0].BaseShaderRegister = 0;
 	descriptorRange[0].NumDescriptors = 1;
@@ -125,28 +126,27 @@ void MyEngine::CreateRootSignature()
 	descriptionRootSignature.pStaticSamplers = staticSamplers;
 	descriptionRootSignature.NumStaticSamplers = _countof(staticSamplers);
 
-	//シリアライズしてバイナリにする
+	// シリアライズしてバイナリにする
 	signatureBlob_ = nullptr;
 	errorBlob_ = nullptr;
 	HRESULT hr;
-	hr = D3D12SerializeRootSignature(&descriptionRootSignature,
-		D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob_, &errorBlob_);
+	hr = D3D12SerializeRootSignature(
+		&descriptionRootSignature, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob_, &errorBlob_);
 
-	if (FAILED(dxCommon_->GetHr()))
-	{
+	if (FAILED(dxCommon_->GetHr())) {
 		Log(reinterpret_cast<char*>(errorBlob_->GetBufferPointer()));
 		assert(false);
 	}
 
-	//バイナリを元に生成
+	// バイナリを元に生成
 	rootSignature_ = nullptr;
-	hr = dxCommon_->GetDevice()->CreateRootSignature(0, signatureBlob_->GetBufferPointer(),
-		signatureBlob_->GetBufferSize(), IID_PPV_ARGS(&rootSignature_));
+	hr = dxCommon_->GetDevice()->CreateRootSignature(
+		0, signatureBlob_->GetBufferPointer(), signatureBlob_->GetBufferSize(),
+		IID_PPV_ARGS(&rootSignature_));
 	assert(SUCCEEDED(hr));
 }
 
-void MyEngine::CreateInputlayOut()
-{
+void MyEngine::CreateInputlayOut() {
 	inputElementDescs_[0].SemanticName = "POSITION";
 	inputElementDescs_[0].SemanticIndex = 0;
 	inputElementDescs_[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
@@ -161,75 +161,87 @@ void MyEngine::CreateInputlayOut()
 	inputLayoutDesc_.NumElements = _countof(inputElementDescs_);
 }
 
-void MyEngine::BlendState()
-{
-	//すべての色要素を書き込む
+void MyEngine::BlendState() {
+	// すべての色要素を書き込む
 	blendDesc_.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
 }
 
-void MyEngine::RasterizerState()
-{
-	//裏面（時計回り）を表示しない
+void MyEngine::RasterizerState() {
+	// 裏面（時計回り）を表示しない
 	rasterizerDesc_.CullMode = D3D12_CULL_MODE_BACK;
 
-	//三角形の中を塗りつぶす
+	// 三角形の中を塗りつぶす
 	rasterizerDesc_.FillMode = D3D12_FILL_MODE_SOLID;
 
-	//Shaderをコンパイルする
-	vertexShaderBlob_ = CompileShader(L"Object3d.VS.hlsl",
-		L"vs_6_0", dxcUtils_, dxcCompiler_, includeHandler_);
+	// Shaderをコンパイルする
+	vertexShaderBlob_ =
+		CompileShader(L"Object3d.VS.hlsl", L"vs_6_0", dxcUtils_, dxcCompiler_, includeHandler_);
 	assert(vertexShaderBlob_ != nullptr);
 
-	pixelShaderBlob_ = CompileShader(L"Object3d.PS.hlsl",
-		L"ps_6_0", dxcUtils_, dxcCompiler_, includeHandler_);
+	pixelShaderBlob_ =
+		CompileShader(L"Object3d.PS.hlsl", L"ps_6_0", dxcUtils_, dxcCompiler_, includeHandler_);
 	assert(pixelShaderBlob_ != nullptr);
 }
 
-void MyEngine::InitializePSO()
-{
+void MyEngine::InitializePSO() {
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc{};
 
-	//RootSignature
+	// RootSignature
 	graphicsPipelineStateDesc.pRootSignature = rootSignature_;
 
-	//Inputlayout
+	// Inputlayout
 	graphicsPipelineStateDesc.InputLayout = inputLayoutDesc_;
 
-	//vertexShader
-	graphicsPipelineStateDesc.VS = { vertexShaderBlob_->GetBufferPointer(),
-		vertexShaderBlob_->GetBufferSize() };
+	// vertexShader
+	graphicsPipelineStateDesc.VS = {
+		vertexShaderBlob_->GetBufferPointer(), vertexShaderBlob_->GetBufferSize() };
 
-	//pixcelShader
-	graphicsPipelineStateDesc.PS = { pixelShaderBlob_->GetBufferPointer(),
-		pixelShaderBlob_->GetBufferSize() };
+	// pixcelShader
+	graphicsPipelineStateDesc.PS = {
+		pixelShaderBlob_->GetBufferPointer(), pixelShaderBlob_->GetBufferSize() };
 
-	//BlendState
+	// BlendState
 	graphicsPipelineStateDesc.BlendState = blendDesc_;
 
-	//rasterizerState
+	// rasterizerState
 	graphicsPipelineStateDesc.RasterizerState = rasterizerDesc_;
 
-	//書き込むRTVの情報
+	// 書き込むRTVの情報
 	graphicsPipelineStateDesc.NumRenderTargets = 1;
 	graphicsPipelineStateDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
 
-	//利用するトポロジ（形状）のタイプ。三角形
+	// 利用するトポロジ（形状）のタイプ。三角形
 	graphicsPipelineStateDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 
-	//どのように画面に色を打ち込むのかの設定（気にしなくて良い）
+	// どのように画面に色を打ち込むのかの設定（気にしなくて良い）
 	graphicsPipelineStateDesc.SampleDesc.Count = 1;
 	graphicsPipelineStateDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
 
-	//実際に生成
+	// DepthStencilStateの設定
+	D3D12_DEPTH_STENCIL_DESC depthStencilDesc{};
+
+	// Depthの機能を有効化する
+	depthStencilDesc.DepthEnable = true;
+
+	// 書き込みします
+	depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
+
+	// 比較関数はLessEqual。つまり、近ければ描画される
+	depthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+
+	// DepthStencilの設定
+	graphicsPipelineStateDesc.DepthStencilState = depthStencilDesc;
+	graphicsPipelineStateDesc.DSVFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+
+	// 実際に生成
 	graphicsPipelineState_ = nullptr;
-	HRESULT hr = dxCommon_->GetDevice()->CreateGraphicsPipelineState(&graphicsPipelineStateDesc,
-		IID_PPV_ARGS(&graphicsPipelineState_));
+	HRESULT hr = dxCommon_->GetDevice()->CreateGraphicsPipelineState(
+		&graphicsPipelineStateDesc, IID_PPV_ARGS(&graphicsPipelineState_));
 	assert(SUCCEEDED(hr));
 }
 
-void MyEngine::ViewPort()
-{
-	//クライアント領域のサイズと一緒にして画面全体に表示
+void MyEngine::ViewPort() {
+	// クライアント領域のサイズと一緒にして画面全体に表示
 	viewport_.Width = WinApp::kClientWidth;
 	viewport_.Height = WinApp::kClientHeight;
 	viewport_.TopLeftX = 0;
@@ -238,27 +250,27 @@ void MyEngine::ViewPort()
 	viewport_.MaxDepth = 1.0f;
 }
 
-void MyEngine::ScissorRect()
-{
-	//シザー短形
+void MyEngine::ScissorRect() {
+	// シザー短形
 	scissorRect_.left = 0;
 	scissorRect_.right = WinApp::kClientWidth;
 	scissorRect_.top = 0;
 	scissorRect_.bottom = WinApp::kClientHeight;
 }
 
-void MyEngine::Initialize()
-{
-	transform_ = { {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
-	for (int i = 0; i < 5; i++)
-	{
+void MyEngine::Initialize() {
+	transform_ = {
+		{1.0f, 1.0f, 1.0f},
+		{0.0f, 0.0f, 0.0f},
+		{0.0f, 0.0f, 0.0f}
+	};
+	for (int i = 0; i < 5; i++) {
 		triangle_[i] = new Triangle();
 		triangle_[i]->Initialize(dxCommon_, this);
 	}
 }
 
-void MyEngine::Initialization(WinApp* win, const wchar_t* title, int32_t width, int32_t height)
-{
+void MyEngine::Initialization(WinApp* win, const wchar_t* title, int32_t width, int32_t height) {
 	dxCommon_ = new DirectXCommon();
 	dxCommon_->Initialization(win, title, win->kClientWidth, win->kClientHeight);
 
@@ -279,34 +291,29 @@ void MyEngine::Initialization(WinApp* win, const wchar_t* title, int32_t width, 
 	ScissorRect();
 }
 
-
-void MyEngine::BeginFrame()
-{
+void MyEngine::BeginFrame() {
 	triangleCount_ = 0;
 	dxCommon_->PreDraw();
-	//viewportを設定
+	// viewportを設定
 	dxCommon_->GetCommandList()->RSSetViewports(1, &viewport_);
-	//scirssorを設定
+	// scirssorを設定
 	dxCommon_->GetCommandList()->RSSetScissorRects(1, &scissorRect_);
-	//RootSignatureを設定。PS0とは別途設定が必要
+	// RootSignatureを設定。PS0とは別途設定が必要
 	dxCommon_->GetCommandList()->SetGraphicsRootSignature(rootSignature_);
-	//PS0を設定
+	// PS0を設定
 	dxCommon_->GetCommandList()->SetPipelineState(graphicsPipelineState_);
-	//開発用UIの処理
+	// 開発用UIの処理
 	ImGui::ShowDemoWindow();
 }
 
-void MyEngine::EndFrame()
-{
+void MyEngine::EndFrame() {
 	ImGui::Render();
 
 	dxCommon_->PostDraw();
 }
 
-void MyEngine::Release()
-{
-	for (int i = 0; i < kMaxTriangle; i++)
-	{
+void MyEngine::Release() {
+	for (int i = 0; i < kMaxTriangle; i++) {
 		triangle_[i]->Release();
 		textureResource_->Release();
 	}
@@ -314,8 +321,7 @@ void MyEngine::Release()
 	graphicsPipelineState_->Release();
 	signatureBlob_->Release();
 
-	if (errorBlob_)
-	{
+	if (errorBlob_) {
 		errorBlob_->Release();
 	}
 
@@ -325,66 +331,69 @@ void MyEngine::Release()
 	dxCommon_->Release();
 }
 
-void MyEngine::Update()
-{
-	transform_.rotate.y += 0.03f;
+void MyEngine::Update() {
+	transform_.rotate.y += 0.008f;
 	worldMatrix_ = MakeAffineMatrix(transform_.scale, transform_.rotate, transform_.translate);
 }
 
-void MyEngine::DrawTriangle(const Vector4& a, const Vector4& b, const Vector4& c, const Vector4& material)
-{
+void MyEngine::DrawTriangle(
+	const Vector4& a, const Vector4& b, const Vector4& c, const Vector4& material) {
 	triangleCount_++;
 	triangle_[triangleCount_]->Draw(a, b, c, material, worldMatrix_);
 }
 
-DirectX::ScratchImage MyEngine::LoadTexture(const std::string& filePath)
-{
+DirectX::ScratchImage MyEngine::LoadTexture(const std::string& filePath) {
 	DirectX::ScratchImage mipImages = OpenImage(filePath);
 	const DirectX::TexMetadata& metadata = mipImages.GetMetadata();
 	textureResource_ = CreateTextureResource(dxCommon_->GetDevice(), metadata);
 	UploadTexturData(textureResource_, mipImages);
 
-	//metaDataを基にSRVの設定
+	// metaDataを基にSRVの設定
 	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
 	srvDesc.Format = metadata.format;
 	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
 	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
 	srvDesc.Texture2D.MipLevels = UINT(metadata.mipLevels);
 
-	//SRVを作成するDescriptorHeapの場所を決める
+	// SRVを作成するDescriptorHeapの場所を決める
 	textureSrvHandleCPU_ = dxCommon_->GetSrvDescriptorHeap()->GetCPUDescriptorHandleForHeapStart();
 	textureSrvHandleGPU_ = dxCommon_->GetSrvDescriptorHeap()->GetGPUDescriptorHandleForHeapStart();
 
-	//先頭はImGuiが使っているのでその次を使う
-	textureSrvHandleCPU_.ptr += dxCommon_->GetDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
-	textureSrvHandleGPU_.ptr += dxCommon_->GetDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+	// 先頭はImGuiが使っているのでその次を使う
+	textureSrvHandleCPU_.ptr += dxCommon_->GetDevice()->GetDescriptorHandleIncrementSize(
+		D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+	textureSrvHandleGPU_.ptr += dxCommon_->GetDevice()->GetDescriptorHandleIncrementSize(
+		D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
 
-	//SRVの作成
-	dxCommon_->GetDevice()->CreateShaderResourceView(textureResource_, &srvDesc, textureSrvHandleCPU_);
+	// SRVの作成
+	dxCommon_->GetDevice()->CreateShaderResourceView(
+		textureResource_, &srvDesc, textureSrvHandleCPU_);
 
 	return mipImages;
 }
 
-DirectX::ScratchImage MyEngine::OpenImage(const std::string& filePath)
-{
-	//テクスチャファイルを読んでプログラムで扱えるようにする
+DirectX::ScratchImage MyEngine::OpenImage(const std::string& filePath) {
+	// テクスチャファイルを読んでプログラムで扱えるようにする
 	DirectX::ScratchImage image{};
 	std::wstring filePathW = ConvertString(filePath);
-	HRESULT hr = DirectX::LoadFromWICFile(filePathW.c_str(), DirectX::WIC_FLAGS_FORCE_SRGB, nullptr, image);
+	HRESULT hr =
+		DirectX::LoadFromWICFile(filePathW.c_str(), DirectX::WIC_FLAGS_FORCE_SRGB, nullptr, image);
 	assert(SUCCEEDED(hr));
 
-	//ミップマップの生成
+	// ミップマップの生成
 	DirectX::ScratchImage mipImages{};
-	hr = DirectX::GenerateMipMaps(image.GetImages(), image.GetImageCount(), image.GetMetadata(), DirectX::TEX_FILTER_SRGB, 0, mipImages);
+	hr = DirectX::GenerateMipMaps(
+		image.GetImages(), image.GetImageCount(), image.GetMetadata(), DirectX::TEX_FILTER_SRGB, 0,
+		mipImages);
 	assert(SUCCEEDED(hr));
 
-	//ミップマップ付きのデータを返す
+	// ミップマップ付きのデータを返す
 	return mipImages;
 }
 
-ID3D12Resource* MyEngine::CreateTextureResource(ID3D12Device* device, const DirectX::TexMetadata& metadata)
-{
-	//metadataを基にResourceの設定
+ID3D12Resource*
+MyEngine::CreateTextureResource(ID3D12Device* device, const DirectX::TexMetadata& metadata) {
+	// metadataを基にResourceの設定
 	D3D12_RESOURCE_DESC resourceDesc{};
 	resourceDesc.Width = UINT(metadata.width);
 	resourceDesc.Height = UINT(metadata.height);
@@ -394,34 +403,34 @@ ID3D12Resource* MyEngine::CreateTextureResource(ID3D12Device* device, const Dire
 	resourceDesc.SampleDesc.Count = 1;
 	resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION(metadata.dimension);
 
-	//利用するHeapの設定
+	// 利用するHeapの設定
 	D3D12_HEAP_PROPERTIES heapProperties{};
 	heapProperties.Type = D3D12_HEAP_TYPE_CUSTOM;
 	heapProperties.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_WRITE_BACK;
 	heapProperties.MemoryPoolPreference = D3D12_MEMORY_POOL_L0;
 
-	//Resourceの生成
+	// Resourceの生成
 	ID3D12Resource* resource = nullptr;
-	HRESULT hr = device->CreateCommittedResource(&heapProperties, D3D12_HEAP_FLAG_NONE, &resourceDesc,
-		D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&resource));
+	HRESULT hr = device->CreateCommittedResource(
+		&heapProperties, D3D12_HEAP_FLAG_NONE, &resourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr, IID_PPV_ARGS(&resource));
 	assert(SUCCEEDED(hr));
 
 	return resource;
 }
 
-void MyEngine::UploadTexturData(ID3D12Resource* texture, const DirectX::ScratchImage& mipImages)
-{
-	//Meta情報を取得
+void MyEngine::UploadTexturData(ID3D12Resource* texture, const DirectX::ScratchImage& mipImages) {
+	// Meta情報を取得
 	const DirectX::TexMetadata& metadata = mipImages.GetMetadata();
 
-	//全MipMapについて
-	for (size_t mipLevel = 0; mipLevel < metadata.mipLevels; ++mipLevel)
-	{
-		//MipMapLevelを指定して各Imageを取得
+	// 全MipMapについて
+	for (size_t mipLevel = 0; mipLevel < metadata.mipLevels; ++mipLevel) {
+		// MipMapLevelを指定して各Imageを取得
 		const DirectX::Image* img = mipImages.GetImage(mipLevel, 0, 0);
 
-		//Textureに転送
-		HRESULT hr = texture->WriteToSubresource(UINT(mipLevel), nullptr, img->pixels, UINT(img->rowPitch), UINT(img->slicePitch));
+		// Textureに転送
+		HRESULT hr = texture->WriteToSubresource(
+			UINT(mipLevel), nullptr, img->pixels, UINT(img->rowPitch), UINT(img->slicePitch));
 		assert(SUCCEEDED(hr));
 	}
 }
--- String.h
+++ String.h
@@ -4,7 +4,5 @@
 #include <Windows.h>
 
 std::wstring ConvertString(const std::string& str);
-
 std::string ConvertString(const std::wstring& str);
 void Log(const std::string& message);
-

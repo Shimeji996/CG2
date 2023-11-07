81fe538452b4860ad52db82e6d8a7c0376e0a016
Shimeji996 <k022g0021@g.neec.ac.jp>
2023-11-07 10:13:35 +0900

    一旦終わり

--- Sphere.cpp
+++ Sphere.cpp
@@ -8,7 +8,6 @@ void Sphere::Initialize(DirectXCommon* dxCommon, MyEngine* engine)
 	engine_ = engine;
 	kSubDivision = 16;
 	vertexCount = kSubDivision * kSubDivision * 6;
-	modelData = engine_->LoadObjFile("resources/", "plane.obj");
 	SettingVertex();
 	SettingColor();
 	SettingDictionalLight();
@@ -24,57 +23,105 @@ void Sphere::Draw(const Vector4& material, const Transform& transform, uint32_t 
 
 	Matrix4x4 wvpMatrix_ = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
 
-	uvTransformMatrix = MakeScaleMatrix(uvTransformSprite.scale);
-	uvTransformMatrix = Multiply(uvTransformMatrix, MakeRotateZmatrix(uvTransformSprite.rotate.num[2]));
-	uvTransformMatrix = Multiply(uvTransformMatrix, MakeTranslateMatrix(uvTransformSprite.translate));
+	//経度分割一つ分の角度
+	const float kLonEvery = pi * 2.0f / float(kSubDivision);
+	const float kLatEvery = pi / float(kSubDivision);
 
-	* materialData_ = { material,true };
-	materialData_->uvTransform = uvTransformMatrix;
-	*wvpData_ = { wvpMatrix_,worldMatrix };
-	*directionalLight_ = light;
+	//緯度の方向に分割
+	for (uint32_t latIndex = 0; latIndex < kSubDivision; ++latIndex)
+	{
+		float lat = -pi / 2.0f + kLatEvery * latIndex;
+		//経度の方向に分割しながら線を描く
+		for (uint32_t lonIndex = 0; lonIndex < kSubDivision; ++lonIndex)
+		{
+			uint32_t start = (latIndex * kSubDivision + lonIndex) * 6;
+			float lon = lonIndex * kLonEvery;
+			//頂点にデータを入力する　基準点a
+			vertexData_[start].position = { cos(lat) * cos(lon),sin(lat),cos(lat) * sin(lon),1.0f };
+			vertexData_[start].texcoord = { float(lonIndex) / float(kSubDivision),1.0f - float(latIndex) / kSubDivision };
 
-	//VBVを設定
-	dxCommon_->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView);
+			vertexData_[start + 1].position = { cos(lat + kLatEvery) * cos(lon),sin(lat + kLatEvery),cos(lat + kLatEvery) * sin(lon),1.0f };
+			vertexData_[start + 1].texcoord = { vertexData_[start].texcoord.num[0],vertexData_[start].texcoord.num[1] - 1.0f / float(kSubDivision) };
 
-	//形状を設定。PS0に設定しているものとはまた別。同じものを設定する
-	dxCommon_->GetCommandList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+			vertexData_[start + 2].position = { cos(lat) * cos(lon + kLonEvery),sin(lat),cos(lat) * sin(lon + kLonEvery),1.0f };
+			vertexData_[start + 2].texcoord = { vertexData_[start].texcoord.num[0] + 1.0f / float(kSubDivision),vertexData_[start].texcoord.num[1] };
 
-	//マテリアルCBufferの場所を設定
-	dxCommon_->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource_->GetGPUVirtualAddress());
-	dxCommon_->GetCommandList()->SetGraphicsRootConstantBufferView(1, wvpResource_->GetGPUVirtualAddress());
-	dxCommon_->GetCommandList()->SetGraphicsRootConstantBufferView(3, directionalLightResource_->GetGPUVirtualAddress());
+			vertexData_[start + 3].position = { cos(lat) * cos(lon + kLonEvery),sin(lat),cos(lat) * sin(lon + kLonEvery),1.0f };
+			vertexData_[start + 3].texcoord = { vertexData_[start].texcoord.num[0] + 1.0f / float(kSubDivision),vertexData_[start].texcoord.num[1] };
 
-	//SRVのDescriptorTableの先頭を設定。2はrootParameter[2]のこと
-	dxCommon_->GetCommandList()->SetGraphicsRootDescriptorTable(2, engine_->textureSrvHandleGPU_[index]);
+			vertexData_[start + 4].position = { cos(lat + kLatEvery) * cos(lon),sin(lat + kLatEvery),cos(lat + kLatEvery) * sin(lon),1.0f };
+			vertexData_[start + 4].texcoord = { vertexData_[start].texcoord.num[0],vertexData_[start].texcoord.num[1] - 1.0f / float(kSubDivision) };
 
-	//描画
-	//dxCommon_->GetCommandList()->DrawInstanced(vertexCount, 1, 0, 0);
-	dxCommon_->GetCommandList()->DrawInstanced(UINT(modelData.vertices.size()), 1, 0, 0);
+			vertexData_[start + 5].position = { cos(lat + kLatEvery) * cos(lon + kLonEvery),sin(lat + kLatEvery), cos(lat + kLatEvery) * sin(lon + kLonEvery),1.0f };
+			vertexData_[start + 5].texcoord = { vertexData_[start].texcoord.num[0] + 1.0f / float(kSubDivision),vertexData_[start].texcoord.num[1] - 1.0f / float(kSubDivision) };
+
+			vertexData_[start].normal.num[0] = vertexData_[start].position.num[0];
+			vertexData_[start].normal.num[1] = vertexData_[start].position.num[1];
+			vertexData_[start].normal.num[2] = vertexData_[start].position.num[2];
+
+			vertexData_[start + 1].normal.num[0] = vertexData_[start + 1].position.num[0];
+			vertexData_[start + 1].normal.num[1] = vertexData_[start + 1].position.num[1];
+			vertexData_[start + 1].normal.num[2] = vertexData_[start + 1].position.num[2];
+
+			vertexData_[start + 2].normal.num[0] = vertexData_[start + 2].position.num[0];
+			vertexData_[start + 2].normal.num[1] = vertexData_[start + 2].position.num[1];
+			vertexData_[start + 2].normal.num[2] = vertexData_[start + 2].position.num[2];
+
+			vertexData_[start + 3].normal.num[0] = vertexData_[start + 3].position.num[0];
+			vertexData_[start + 3].normal.num[1] = vertexData_[start + 3].position.num[1];
+			vertexData_[start + 3].normal.num[2] = vertexData_[start + 3].position.num[2];
+
+			vertexData_[start + 4].normal.num[0] = vertexData_[start + 4].position.num[0];
+			vertexData_[start + 4].normal.num[1] = vertexData_[start + 4].position.num[1];
+			vertexData_[start + 4].normal.num[2] = vertexData_[start + 4].position.num[2];
+
+			vertexData_[start + 5].normal.num[0] = vertexData_[start + 5].position.num[0];
+			vertexData_[start + 5].normal.num[1] = vertexData_[start + 5].position.num[1];
+			vertexData_[start + 5].normal.num[2] = vertexData_[start + 5].position.num[2];
+			*materialData_ = { material,true };
+			*wvpData_ = { wvpMatrix_,worldMatrix };
+			*directionalLight_ = light;
+
+			//VBVを設定
+			dxCommon_->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView);
+
+			//形状を設定。PS0に設定しているものとはまた別。同じものを設定する
+			dxCommon_->GetCommandList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+			//マテリアルCBufferの場所を設定
+			dxCommon_->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource_->GetGPUVirtualAddress());
+			dxCommon_->GetCommandList()->SetGraphicsRootConstantBufferView(1, wvpResource_->GetGPUVirtualAddress());
+			dxCommon_->GetCommandList()->SetGraphicsRootConstantBufferView(3, directionalLightResource_->GetGPUVirtualAddress());
+
+			//SRVのDescriptorTableの先頭を設定。2はrootParameter[2]のこと
+			dxCommon_->GetCommandList()->SetGraphicsRootDescriptorTable(2, engine_->textureSrvHandleGPU_[index]);
+
+			//描画
+			dxCommon_->GetCommandList()->DrawInstanced(vertexCount, 1, 0, 0);
+		}
+	}
 }
 
 void Sphere::Finalize()
 {
-	//	vertexResource->Release();
-	//	materialResource_->Release();
-	//	directionalLightResource_->Release();
-	//	wvpResource_->Release();
+	vertexResource->Release();
+	materialResource_->Release();
+	directionalLightResource_->Release();
+	wvpResource_->Release();
 }
 
 void Sphere::SettingVertex()
 {
-	//vertexResource = dxCommon_->CreateBufferResource(dxCommon_->GetDevice(), sizeof(VertexData) * vertexCount);
-	vertexResource = dxCommon_->CreateBufferResource(dxCommon_->GetDevice(), sizeof(VertexData) * modelData.vertices.size());
+	vertexResource = dxCommon_->CreateBufferResource(dxCommon_->GetDevice(), sizeof(VertexData) * vertexCount);
+
 	//リソースの先頭のアドレスから使う
 	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
 
-	//vertexBufferView.SizeInBytes = sizeof(VertexData) * vertexCount;
-	vertexBufferView.SizeInBytes = UINT(sizeof(VertexData) * modelData.vertices.size());
+	vertexBufferView.SizeInBytes = sizeof(VertexData) * vertexCount;
 
 	vertexBufferView.StrideInBytes = sizeof(VertexData);
 
 	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData_));
-
-	std::memcpy(vertexData_, modelData.vertices.data(), sizeof(VertexData) * modelData.vertices.size());
 }
 
 void Sphere::TransformMatrix()
@@ -86,11 +133,9 @@ void Sphere::TransformMatrix()
 
 void Sphere::SettingColor()
 {
-	materialResource_ = dxCommon_->CreateBufferResource(dxCommon_->GetDevice(), sizeof(Material));
+	materialResource_ = dxCommon_->CreateBufferResource(dxCommon_->GetDevice(), sizeof(VertexData));
 
 	materialResource_->Map(0, nullptr, reinterpret_cast<void**>(&materialData_));
-
-	materialData_->uvTransform = MakeIdentity4x4();
 }
 
 void Sphere::SettingDictionalLight()

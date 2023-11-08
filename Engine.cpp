6fb20683b67f7a448e38b1764b10c862193b653a
Shimeji996 <k022g0021@g.neec.ac.jp>
2023-11-08 10:39:10 +0900

    GEの課題終わり

--- Engine.cpp
+++ Engine.cpp
@@ -188,12 +188,12 @@ void MyEngine::RasterizerState()
 	rasterizerDesc_.FillMode = D3D12_FILL_MODE_SOLID;
 
 	//Shaderをコンパイルする
-	vertexShaderBlob_ = CompileShader(L"Object3d.VS.hlsl",
+	vertexShaderBlob_ = CompileShader(L"resources/shaders/Object3d.VS.hlsl",
 		L"vs_6_0", dxcUtils_, dxcCompiler_, includeHandler_);
 	assert(vertexShaderBlob_ != nullptr);
 
 
-	pixelShaderBlob_ = CompileShader(L"Object3d.PS.hlsl",
+	pixelShaderBlob_ = CompileShader(L"resources/shaders/Object3d.PS.hlsl",
 		L"ps_6_0", dxcUtils_, dxcCompiler_, includeHandler_);
 	assert(pixelShaderBlob_ != nullptr);
 }
